81ed9a24d24604215a0f7e5d50f145c45d63697e
Afrijal Rasya Putra <afrijalrasyap@gmail.com>
2025-07-07 14:35:25 +0800

    Update 09_performance_analysis.sql

--- scripts/09_performance_analysis.sql
+++ scripts/09_performance_analysis.sql
@@ -1,21 +1,21 @@
 /*
 ===============================================================================
-Performance Analysis (Year-over-Year, Month-over-Month)
+Analisis Kinerja (Tahun ke Tahun, Bulan ke Bulan)
 ===============================================================================
-Purpose:
-    - To measure the performance of products, customers, or regions over time.
-    - For benchmarking and identifying high-performing entities.
-    - To track yearly trends and growth.
+Tujuan
+    - Untuk mengukur performa produk, pelanggan, atau wilayah dari waktu ke waktu.
+    - Untuk Membandingksn dan mengidentifikasi entitas berkinerja tinggi.
+    - Untuk melacak tren dan pertumbuhan tahunan.
 
-SQL Functions Used:
-    - LAG(): Accesses data from previous rows.
-    - AVG() OVER(): Computes average values within partitions.
-    - CASE: Defines conditional logic for trend analysis.
+Fungsi SQL yang digunakan:
+    - LAG(): Accesses data dari baris sebelumnya
+    - AVG() OVER(): Menghitung nilai rata-rata dalam partisi.
+    - CASE: Mendefinisikan conditional logic untuk analisis tren.
 ===============================================================================
 */
 
-/* Analyze the yearly performance of products by comparing their sales 
-to both the average sales performance of the product and the previous year's sales */
+/* Menganalisis kinerja tahunan produk dengan membandingkan penjualannya
+dengan kinerja penjualan rata-rata produk dan penjualan tahun sebelumnya */
 WITH yearly_product_sales AS (
     SELECT
         YEAR(f.order_date) AS order_year,
